8046177752072e69ba2464a2b26d828950151bf6
syldrew <syl_peke14@hotmail.com>
2023-09-16 21:12:22 -0500

    Fetching Appointment Data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,37 +1,46 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
 import Panel from "./Panel";
-
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+} from "helpers/selectors";
 
 import classnames from "classnames";
 
 const data = [
-    {
-      id: 1,
-      label: "Total Photos",
-      value: 10
-    },
-    {
-      id: 2,
-      label: "Total Topics",
-      value: 4
-    },
-    {
-      id: 3,
-      label: "User with the most uploads",
-      value: "Allison Saeng"
-    },
-    {
-      id: 4,
-      label: "User with the least uploads",
-      value: "Lukas Souza"
-    }
-  
-  ];
-  
+  {
+    id: 1,
+    label: "Total Photos",
+    getValue: getTotalPhotos
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    getValue: getTotalTopics
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    getValue: getUserWithMostUploads
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    getValue: getUserWithLeastUploads
+  }
+
+];
 
 class Dashboard extends Component {
- 
+  state = {
+    loading: true,
+    focused: null,
+    photos: [],
+    topics: []
+  };
 
   selectPanel(id) {
     this.setState(previousState => ({
@@ -41,8 +50,18 @@ class Dashboard extends Component {
 
   componentDidMount() {
     const focused = JSON.parse(localStorage.getItem("focused"));
-    
-    
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(response => response.json()));
+    Promise.all(urlsPromise)
+      .then(([photos, topics]) => {
+        this.setState({
+          loading: false,
+          photos: photos,
+          topics: topics
+        });
+      });
     if (focused) {
       this.setState({ focused });
     }
